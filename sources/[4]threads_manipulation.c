9238b8c6affc28b9e9fccb8d8f402aeb814158c4
motero <motero@student.42.fr>
2022-12-07 23:35:45 +0100

    create thread to test

--- sources/[4]threads_manipulation.c
+++ sources/[4]threads_manipulation.c
@@ -6,7 +6,7 @@
 /*   By: motero <motero@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 11:06:37 by motero            #+#    #+#             */
-/*   Updated: 2022/12/07 22:21:08 by motero           ###   ########.fr       */
+/*   Updated: 2022/12/07 23:35:23 by motero           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,12 @@
 
 void	ft_create_threads(t_arguments args, t_list_item *list)
 {
-	pthread_t	*philosopher;
-	int			i;
-
-	(void)list;
-	philosopher = malloc(sizeof(pthread_t) * args.number_of_philosophers);
-	if (!philosopher)
-		return ;
-	i = 0;
-	while (i < args.number_of_philosophers)
+	t_list_item *current = list;
+	while (current != NULL)
 	{
-		//pthread_create(&philosopher[i], NULL, philosopher_behavior, get_philosopher(list, i + 1));
-		i++;
+		if (current->type == PHILOSOPHER) {
+		current->thread = malloc(sizeof(pthread_t));
+		pthread_create(current->thread, NULL, function, current);
 	}
+	current = current->next;
 }
