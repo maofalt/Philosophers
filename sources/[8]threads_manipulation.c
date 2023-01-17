5cac6cebe2b94831a6fdac720bdd61d63d713072
motero <motero@student.42.fr>
2023-01-17 19:50:10 +0100

    ft_create_threads funcion cleaned but no at norm yet. Not have to mdoify the hilosopphers behavior  ile to reflect the changes in the sturcture

--- sources/[8]threads_manipulation.c
+++ sources/[8]threads_manipulation.c
@@ -20,23 +20,29 @@ void	ft_create_threads(t_list_item *list)
 	int				*someone_died;
 	pthread_t		*monitor_thread;
 
+	monitor_thread = malloc(sizeof(pthread_t));
+	if (!monitor_thread)
+		return ;
 	current = list;
 	gettimeofday(&start, NULL);
+	*nbr_philo_full = 0;
+	*someone_died = 0;
 	while (current)
 	{
 		current->info->timestamps->start = start;
 		current->info->timestamps->start_last_meal = start;
-		*nbr_philo_full = 0;
-		*someone_died = 0;
-		thread_info->nbr_philo_full = nbr_philo_full;
-		thread_info->someone_died = someone_died;
+		current->info->nbr_philo_full = nbr_philo_full;
+		current->info->someone_died = someone_died;
 		if (current->type == PHILOSOPHER)
-			pthread_create(current->thread, NULL, philosopher_thread, thread_info);
+		{
+			pthread_create(current->thread, NULL,
+				philosopher_thread, thread_info);
+		}
 		current = current->next;
 		if (current == list || current->args.number_of_philosophers == 1)
 			break ;
 	}
-	monitor_thread = malloc(sizeof(pthread_t));
 	thread_info->item = list;
 	pthread_create(monitor_thread, NULL, monitor_philosophers, thread_info);
 }
+s
